ca42a2aa644310d24fc37599d3abeffad44bbc03
Nate Gelle <gelle.nate@gmail.com>
2018-07-05 16:34:28 -0500

    initial branch commit

---
+++ src/redux/reducers/shelfReducer.js
@@ -0,0 +1,20 @@
+import { combineReducers } from 'redux';
+import axios from 'axios';
+
+const shelf = (state = [], action) => {
+    if(action.type === 'ADD_SPICE') {
+        state = [...state, action.payload];
+    } else if(action.type === 'GET_SPICES') {
+        axios.get('/api/shelf').then(response => {
+            console.log(response.data.rows);
+            state = response.data.rows;
+        }).catch(err => {
+            console.log({err});
+        })
+    }
+    return state;
+}
+
+export default combineReducers({
+  shelf,
+});
